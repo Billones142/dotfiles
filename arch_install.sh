97fc30f313566919ca198198115738cba79944cd
Billones142 <stefanomerinoderui@gmail.com>
2026-04-14 22:23:00 -0300

    script para preparar programas para instalar config en arch

---
+++ arch_install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#TODO: habilitar colores en git
+
+# necesario
+sudo pacman -S nmap swaync swayosd kdeconnect sway flatpak firewalld stow tailscale htop nvtop rofi xcb-util-cursor xorg-xhost nss-mdns wget python-reportlab python-pyqt5 breeze-icons qt5ct qt6ct gsfonts cantarell-fonts ttf-jetbrains-mono-nerd brightnessctl
+
+xhost +local:root
+sudo gpasswd -a $USER input
+
+# GUI
+sudo pacman -S firewalld-config firewalld-applet nm-connection-editor sddm dolphin partitionmanager 
+
+#TODO: si tiene bluetooth
+#bluez bluez-utils blueman
+
+
+# Otros
+sudo pacman -S docker
+
+sudo pacman -S --needed base-devel git
+
+
+yay -S rofi-power-menu brave-browser blesh sugar-candy obsidian
+
+cd ~
+git clone https://github.com/Billones142/dotfiles
+
+# firewall
+#
+
+# modo oscuro
+#TODO: agregar cambios a qt5/6
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+
+#TODO: habilitar sugar-candy en sddm
+
+sudo systemctl enable --now firewalld
+sudo systemctl enable --now avahi-daemon
+
+systemctl --user enable --now hyprpolkitagent
+systemctl --user enable --now blueman-applet
+
+#TODO: activar servicio de detecion de mdns
+# hosts: mymachines **mdns_minimal [NOTFOUND=return]** resolve [!UNAVAIL=return] files myhostname dns
+#sudo nvim /etc/nsswitch.conf
+sudo firewall-cmd --add-service=mdns --permanent
+sudo firewall-cmd --reload
