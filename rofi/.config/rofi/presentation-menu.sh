01952ac3c50e4af3a8f6e9ae7ceacf85fe01773c
Billones142 <stefanomerinoderui@gmail.com>
2026-06-07 01:44:59 -0300

    hypr: script de cambio de output para presentaciones actualizado para utilizar la configuracion de lua

--- rofi/.config/rofi/presentation-menu.sh
+++ rofi/.config/rofi/presentation-menu.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Rutas de archivos (usando rutas absolutas para Rofi)
-CONFIG_FILE="/home/stefano/.config/hypr/monitors_presentation.conf"
+CONFIG_FILE="$HOME/.config/hypr/monitors_presentation.lua"
 MAIN_OUTPUT="desc:AU Optronics 0x4A90"
 HDMI_OUTPUT="HDMI-A-1"
 
@@ -23,28 +23,28 @@ else
         "$OPC_MIRROR")
             # Escribe la configuración de espejo
             cat <<EOF > "$CONFIG_FILE"
-monitorv2 {
-    output = $HDMI_OUTPUT
-    mirror = $MAIN_OUTPUT
-    position = auto
-    scale = 1
-    bitdepth = 8
-    vrr = 1
-}
+hl.monitor({
+    output = "$HDMI_OUTPUT",
+    mirror = "$MAIN_OUTPUT",
+    position = "auto",
+    scale = 1,
+    bitdepth = 8,
+    vrr = 1,
+})
 EOF
             notify-send "Monitor" "Modo Espejo activado"
             ;;
         "$OPC_EXTEND")
             # Escribe la configuración extendida (HDMI a la derecha por defecto)
             cat <<EOF > "$CONFIG_FILE"
-monitorv2 {
-    output = $HDMI_OUTPUT
-    mode = preferred
-    position = auto
-    scale = 1
-    bitdepth = 8
-    vrr = 1
-}
+hl.monitor({
+    output = "$HDMI_OUTPUT",
+    mode = "preferred",
+    position = "auto",
+    scale = 1,
+    bitdepth = 8,
+    vrr = 1,
+})
 EOF
             notify-send "Monitor" "Pantalla extendida activada"
             ;;
