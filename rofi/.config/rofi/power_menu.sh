634c30a808395e0e6de36e85baccbf0d25e7203f
Billones142 <stefanomerinoderui@gmail.com>
2026-09-10 21:47:22 -0300

    rofi: agregado reinicio rapido y funcion para simplificar uso de hyprshutdown

--- rofi/.config/rofi/power_menu.sh
+++ rofi/.config/rofi/power_menu.sh
@@ -3,30 +3,38 @@
 # Definimos las opciones
 OPC_POWEROFF="󰐥 Apagar"
 OPC_REBOOT="󰜉 Reiniciar"
+OPC_SOFTREBOOT="󰜉 Reinicio rapido"
 OPC_LOGOUT="󰍃 Cerrar Sesión"
 OPC_SUSPEND="󰤄 Suspender"
 OPC_SWITCH="󰍉 Cambiar Usuario"
 
+hyprshutdown_command () {
+  setsid hyprshutdown  --top-label "$1" --post-cmd "$2" > /dev/null 2>&1 &
+}
+
 if [ -z "$1" ]; then
     # Listar opciones para Rofi
-    echo -e "$OPC_POWEROFF\n$OPC_REBOOT\n$OPC_LOGOUT\n$OPC_SUSPEND\n$OPC_SWITCH"
+    echo -e "$OPC_POWEROFF\n$OPC_REBOOT\n$OPC_SOFTREBOOT\n$OPC_LOGOUT\n$OPC_SUSPEND\n$OPC_SWITCH"
 else
     #pkill rofi
     # Ejecutar acción según la selección
     case "$1" in
         "$OPC_POWEROFF")
-	    (setsid hyprshutdown  --top-label "Shutting down..." --post-cmd "systemctl poweroff" > /dev/null 2>&1 &)
+	    (hyprshutdown_command "Shutting down..." "systemctl poweroff")
             ;;
         "$OPC_REBOOT")
-	    (setsid hyprshutdown --top-label "Rebooting..." --post-cmd "systemctl reboot" > /dev/null 2>&1 &)
+	    (hyprshutdown_command "Rebooting..." "systemctl reboot")
+            ;;
+        "$OPC_SODTREBOOT")
+	    (hyprshutdown_command "Rebooting..." "systemctl soft-reboot")
             ;;
         "$OPC_LOGOUT")
-	    (setsid hyprshutdown  --top-label "Loging out..." > /dev/null 2>&1 &)
+	    (hyprshutdown_command "Loging out..." "")
             ;;
         "$OPC_SUSPEND")
             # Ejemplo de uso de --post-cmd para bloquear antes de suspender
-            #systemctl hybrid-sleep &
-            systemctl suspend-then-hibernate &
+            systemctl hybrid-sleep &
+            #systemctl suspend-then-hibernate &
             ;;
         "$OPC_SWITCH")
             loginctl lock-session ; busctl call org.freedesktop.DisplayManager /org/freedesktop/DisplayManager/Seat0 org.freedesktop.DisplayManager.Seat SwitchToGreeter
